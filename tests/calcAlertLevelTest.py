cca682c091fafe8730de85ce7ed21d2ea21b97ec
Randy Sorensen <gameforge1@gmail.com>
2015-11-21 15:50:27 -0700

    Made some revisions to our alert level unit tests.
    An aircraft's "location" is actually its distance, on each axis, from "ownShip". This is true on all axes including Z. The previous tests were setting ownShip's Z to e.g. 10k feet and the test aircraft's Z to e.g. 12k5 feet, when ownShip's should be 0 and the other aircraft's should be 2k5.
    
    That translation all gets performed by the ADSB processor.

--- tests/calcAlertLevelTest.py
+++ tests/calcAlertLevelTest.py
@@ -7,37 +7,32 @@ from src.Aircraft import *
 class AircraftcalcAlertLevelTest(unittest.TestCase):
     def test_alertLv1(self):
         main = MainClass()
-        main.setOwnShip(60000,60000,10000,50,30,40,0,1)
-        testAircraft = Aircraft(0,0,18000,50,30,40,0,2)
-        main.ownShip.calcAlertLevel(testAircraft)
+        testAircraft = Aircraft(60000,60000,8000,50,30,40,0,1)
+        testAircraft.calcAlertLevel(main.ownShip)
         self.assertEquals(testAircraft.getAlertLevel(), 1)
 
     def test_alertLv2(self):
         main = MainClass()
-        main.setOwnShip(30000,30000,10000,50,30,40,0,1)
-        testAircraft = Aircraft(0,0,12500,50,30,40,0,2)
-        main.ownShip.calcAlertLevel(testAircraft)
+        testAircraft = Aircraft(30000,30000,2500,50,30,40,0,1)
+        testAircraft.calcAlertLevel(main.ownShip)
         self.assertEquals(testAircraft.getAlertLevel(), 2)
 
     def test_alertLv3(self):
         main = MainClass()
-        main.setOwnShip(25000,25000,10000,50,30,40,0,1)
-        testAircraft = Aircraft(0,0,12500,50,30,40,0,2)
-        main.ownShip.calcAlertLevel(testAircraft)
+        testAircraft = Aircraft(25000,25000,2500,50,30,40,0,1)
+        testAircraft.calcAlertLevel(main.ownShip)
         self.assertEquals(testAircraft.getAlertLevel(), 3)
 
     def test_alertLv4(self):
         main = MainClass()
-        main.setOwnShip(20000,20000,10000,50,30,40,0,1)
-        testAircraft = Aircraft(0,0,11600,50,30,40,0,2)
-        main.ownShip.calcAlertLevel(testAircraft)
+        testAircraft = Aircraft(20000,20000,1600,50,30,40,0,1)
+        testAircraft.calcAlertLevel(main.ownShip)
         self.assertEquals(testAircraft.getAlertLevel(), 4)
 
     def test_alertLv5(self):
         main = MainClass()
-        main.setOwnShip(15000,15000,10000,50,30,40,0,1)
-        testAircraft = Aircraft(0,0,11000,50,30,40,0,2)
-        main.ownShip.calcAlertLevel(testAircraft)
+        testAircraft = Aircraft(15000,15000,1000,50,30,40,0,1)
+        testAircraft.calcAlertLevel(main.ownShip)
         self.assertEquals(testAircraft.getAlertLevel(), 5)
 
 if __name__ == "__main__":
